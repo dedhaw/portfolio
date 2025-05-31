fba1cb8d747ceeb2ecd5bd99236a26ff2902f081
dedhaw <160305533+dedhaw@users.noreply.github.com>
2025-05-30 20:19:30 -0700

    Ipad mac OS tabs

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -160,61 +160,61 @@ export default function AboutMe({id}: AboutMeProps) {
                                 id="about-me"
                                 title="contact"
                                 content={<AboutTab />} 
-                                width={580}
-                                height={500}
+                                width={441}
+                                height={520}
                                 startingXPosition={20}
                                 startingYPosition={2}
                                 baseWidth={ipadBW}
                                 baseHeight={ipadBH}
                             />
-                            {/* <MacOS 
+                            <MacOS 
                                 id="paragraph"
                                 title="about me"
                                 content={<Paragraph />}
                                 backgroundColor="light-gray"
-                                width={380}
-                                height={500}
-                                startingXPosition={620}
-                                startingYPosition={2}
+                                width={441}
+                                height={430}
+                                startingXPosition={20}
+                                startingYPosition={550}
                             />
                             <MacOS 
                                 id="additonal-info"
                                 // title="where I've worked"
                                 // content={<WorkStyle />}
                                 backgroundColor="light-gray"
-                                width={550}
-                                height={209}
-                                startingXPosition={1014}
-                                startingYPosition={2}
+                                width={515}
+                                height={300}
+                                startingXPosition={485}
+                                startingYPosition={400}
                             />
                             <MacOS 
                                 id="highlight"
                                 title="highlighting skills"
                                 content={<Highlight />}
-                                width={550}
-                                height={273}
-                                startingXPosition={1014}
-                                startingYPosition={225}
+                                width={515}
+                                height={250}
+                                startingXPosition={485}
+                                startingYPosition={725}
                             />
                             <MacOS 
                                 id="skills"
                                 title="skills and tools"
                                 content={<Skills />}
-                                width={1160}
-                                height={347}
-                                startingXPosition={405}
-                                startingYPosition={519}
+                                width={980}
+                                height={340}
+                                startingXPosition={20}
+                                startingYPosition={1000}
                             />
                             <MacOS 
                                 id="stack"
                                 title="my stack"
                                 content={<MyStack />}
                                 backgroundColor="light-gray"
-                                width={370}
-                                height={345}
-                                startingXPosition={20}
-                                startingYPosition={519}
-                            /> */}
+                                width={515}
+                                height={380}
+                                startingXPosition={485}
+                                startingYPosition={2}
+                            />
                         </>
                     )}
                 </MacOSContainer>
