12a68bf66b44cebd473869cb5089650c3e18cce5
dedhaw <160305533+dedhaw@users.noreply.github.com>
2025-06-22 12:33:43 +1000

    Moved to utility file.
    Will be used with custom animations in the work experience tab

--- src/components/SkillDescription.tsx
+++ src/components/SkillDescription.tsx
@@ -1,16 +1,4 @@
-import { useState, useEffect } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-    .skill {
-        opacity: 0;
-        transition: opacity 0.6s ease-in;
-    }
-
-    .skill.visible {
-        opacity: 1;
-    }
-`;
+import Animate from "../utils/HighlightFadeIn";
 
 interface SkillProps {
     id: string;
@@ -21,19 +9,9 @@ export default function Skill({
     id,
     description
 }: SkillProps) {
-    const [visible, setVisible] = useState(false);
-    
-    useEffect(() => {
-        setVisible(false);
-        const t = window.setTimeout(() => setVisible(true), 20);
-        return () => window.clearTimeout(t);
-    }, [id]);
-
   return (
-    <Container>
-        <div className={`skill${visible ? ' visible' : ''}`}>
-            <p>{description}</p>
-        </div>
-    </Container>
+    <Animate trigger={id}>
+        <p>{description}</p>
+    </Animate>
   )
 }
